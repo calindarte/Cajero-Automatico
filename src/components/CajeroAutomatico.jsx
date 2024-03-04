662ab41621a1ede3d7fd78c689fca29db650efc5
calindarte <carlosalbertolindartesanto@gmail.com>
2024-03-03 20:02:18 -0500

    validaciones

--- src/components/CajeroAutomatico.jsx
+++ src/components/CajeroAutomatico.jsx
@@ -82,17 +82,17 @@ const  CajeroAutomatico = () =>{
   };
 
   return (
-    <div className=" md:border-[30px] border-[14px] border-x-slate-900   bg-amber-500 " >
+    <div className=" md:border-[30px] border-[20px] md:p-20 px-8 py-14  border-x-slate-900   bg-amber-500 " >
         
-      <div className="flex flex-col  m-4  md:m-20 border-solid border-[10px] md:border-[40px] border-y-slate-600 bg-blue-200  shadow-lg items-center p-6 gap-6 rounded-xl">
+      <div className="flex flex-col border-solid border-[15px] md:border-[40px] border-y-slate-600 bg-blue-200  shadow-lg items-center md:p-6 px-4 py-12 md:gap-6 gap-8 rounded-xl">
 
 
-        <div className="flex justify-between items-center md:px-8">
+        <div className="flex justify-between items-center md:px-8 ">
         <img src={logoCajero} alt="" className="object-fill w-[10%] h-auto" />
-          <h1 className="font-semibold md:text-4xl text-xl">Cajero Automático</h1>
+          <h1 className="font-bold text-center  font-serif md:text-4xl text-lg ">Cajero Automático</h1>
         <img src={logoDinero} alt="" className="object-fill w-[10%] h-auto" />
         </div>
-        <form onSubmit={handleSubmit} className="flex flex-col gap-8">
+        <form onSubmit={handleSubmit} className="flex flex-col md:gap-8 gap-14">
           <div className="flex flex-col md:flex-row gap-4  items-start md:items-center ">
             <label htmlFor="monto" className="font-medium md:text-lg ">
               Monto a Retirar:
@@ -106,9 +106,9 @@ const  CajeroAutomatico = () =>{
               className=" rounded-md px-2 focus:outline-none focus:ring-sky-500 focus:ring-2"
             />
           </div>
-          <ul className="md:text-lg flex flex-col md:gap-8 gap-10  ">
+          <ul className="md:text-lg flex flex-col md:gap-8 gap-16  ">
             {billetes.map((billete, index) => (
-              <li key={index} className="flex  gap-4 flex-wrap">
+              <li key={index} className="flex  gap-5 flex-wrap">
                 <span className="font-medium">
                   Billetes de ${billete} :
                 </span>{" "}
@@ -124,7 +124,7 @@ const  CajeroAutomatico = () =>{
               </li>
             ))}
           </ul>
-          <div className="flex flex-col md:flex-row md:gap-3 gap-5 justify-center  ">
+          <div className="flex flex-col md:flex-row md:gap-3 gap-9  justify-center  ">
             <button
               type="submit"
               className="bg-green-500 hover:bg-green-600 text-gray-50 px-10 py-2 rounded-full shadow-md"
